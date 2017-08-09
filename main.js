82e962e0fc512a0a3e495d90470462758ec45e14
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-09 13:15:36 -0400

    clicking a button does something

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("talking");
+
+var numbers = Array.prototype.slice.call(document.getElementsByClassName("number"), 0);
+
+var operators = Array.prototype.slice.call(document.getElementsByClassName("operator"), 0);
+
+var buttons = document.getElementsByClassName('button')
+
+let equals = document.getElementById('equals');
+equals.addEventListener('click', calculate, false);
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', inputNumbers, false);
+  }
+
+  function inputNumbers() {
+    console.log("ack!");
+  }
+
+function calculate(){
+  let input = document.getElementById('answer').value;
+  console.log(answer);
+}

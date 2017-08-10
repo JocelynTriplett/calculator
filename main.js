3396b2d1b1b42f5a2c41b4ca6a9d254ece500e98
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-10 11:27:04 -0400

    buttons change color when clicked

--- main.js
+++ main.js
@@ -14,16 +14,22 @@ clear.addEventListener('click', clearScreen, false);
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', inputNumbers, false);
-  }
+}
 
   var input = '';
   function inputNumbers() {
+    this.style.backgroundColor = '#DFD561';
+    console.log(this);
+    current_button = this;
     button_value = this.textContent;
     input += button_value;
     answer = document.createTextNode(input);
     screen = document.getElementById('answer');
     screen.innerHTML = '';
     screen.appendChild(answer);
+    setTimeout(function() {
+        current_button.style.backgroundColor = '#36AC2F';
+    },300);
   }
 
 function calculate(){

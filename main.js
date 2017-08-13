c27703da5900f34caf8a3c3889cba1d8e7c927f9
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-13 18:03:45 -0400

    calculate square root

--- main.js
+++ main.js
@@ -36,10 +36,21 @@ for (let i = 0; i < buttons.length; i++) {
   }
 
 function calculate(){
+  if (input.includes("√")) {
+    console.log("there's a square root here");
+    //var String=input.substring(input.lastIndexOf("√")+1,input.lastIndexOf(" "));
+    var string= input.substr(0, input.indexOf('√'));
+    let result = Math.sqrt(string);
+    screen.innerHTML = result;
+    console.log("result is: "+result);
+    input = result;
+  }
+  else {
   let result = eval(input);
   screen.innerHTML = result;
   console.log("result is: "+result);
   input = result;
+  }
 }
 
 function clearScreen() {

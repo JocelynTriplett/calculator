ea61ac4c982b10d8fc6c8a576c22f0c49849d3e2
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-13 18:07:59 -0400

    cleanup

--- main.js
+++ main.js
@@ -13,47 +13,46 @@ let clear = document.getElementById('C');
 clear.addEventListener('click', clearScreen, false);
 
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', inputNumbers, false);
+  buttons[i].addEventListener('click', inputNumbers, false);
 }
 
-  var input = '';
-  function inputNumbers() {
-    this.style.backgroundColor = '#DFD561';
-    console.log(this);
-    current_button = this;
-    button_value = this.getAttribute("display");
-    console.log(button_value);
-    input += button_value;
-    answer = document.createTextNode(input);
-    screen = document.getElementById('answer');
-    screen.innerHTML = '';
-    screen.appendChild(answer);
-    setTimeout(function() {
-      if (current_button.classList.contains("operator")){
-        current_button.style.backgroundColor = 'orange';}
-        else current_button.style.backgroundColor = 'gray';
+var input = '';
+function inputNumbers() {
+  this.style.backgroundColor = '#DFD561';
+  console.log(this);
+  current_button = this;
+  button_value = this.getAttribute("display");
+  console.log(button_value);
+  input += button_value;
+  answer = document.createTextNode(input);
+  screen = document.getElementById('answer');
+  screen.innerHTML = '';
+  screen.appendChild(answer);
+  setTimeout(function() {
+    if (current_button.classList.contains("operator")){
+      current_button.style.backgroundColor = 'orange';}
+      else current_button.style.backgroundColor = 'gray';
     },100);
   }
-
-function calculate(){
-  if (input.includes("√")) {
-    console.log("there's a square root here");
-    //var String=input.substring(input.lastIndexOf("√")+1,input.lastIndexOf(" "));
-    var string= input.substr(0, input.indexOf('√'));
-    let result = Math.sqrt(string);
-    screen.innerHTML = result;
-    console.log("result is: "+result);
-    input = result;
+  
+  function calculate(){
+    if (input.includes("√")) {
+      console.log("there's a square root here");
+      var string= input.substr(0, input.indexOf('√'));
+      let result = Math.sqrt(string);
+      screen.innerHTML = result;
+      console.log("result is: "+result);
+      input = result;
+    }
+    else {
+      let result = eval(input);
+      screen.innerHTML = result;
+      console.log("result is: "+result);
+      input = result;
+    }
   }
-  else {
-  let result = eval(input);
-  screen.innerHTML = result;
-  console.log("result is: "+result);
-  input = result;
-  }
-}
 
-function clearScreen() {
-  screen.innerHTML = '';
-  input = '';
-}
+  function clearScreen() {
+    screen.innerHTML = '';
+    input = '';
+  }
